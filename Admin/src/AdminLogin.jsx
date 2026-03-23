5695071491d64aae4b1480eaf9404dfe1412869d
Muhammad Sinan C M <muhammadsinancm30@gmail.com>
2026-03-23 19:58:20 +0530

    final

--- Admin/src/AdminLogin.jsx
+++ Admin/src/AdminLogin.jsx
@@ -3,27 +3,27 @@ import axios from 'axios'
 import { useState } from 'react';
 import { backendURL } from './App';
 
-function AdminLogin({setToken}) {
+function AdminLogin({ setToken }) {
 
-    const [adminEmail, setAdminEmail] = useState('')
-    const [adminPassword, setAdminPassword] = useState('')
+  const [adminEmail, setAdminEmail] = useState('')
+  const [adminPassword, setAdminPassword] = useState('')
 
   const UserSubmit = async (event) => {
     console.log(adminEmail);
     console.log(adminPassword);
-    
-    try {
-        event.preventDefault();
 
-        const responce = await axios.post(backendURL + '/api/admin/login', {adminEmail, adminPassword})
-        if (responce.data.success) {
-            setToken(responce.data.token)
-        } else {
-            console.log(responce.data.message);
-        }
-        
+    try {
+      event.preventDefault();
+
+      const responce = await axios.post(backendURL + '/api/admin/login', { adminEmail, adminPassword })
+      if (responce.data.success) {
+        setToken(responce.data.token)
+      } else {
+        console.log(responce.data.message);
+      }
+
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
     }
   }
 
@@ -31,11 +31,11 @@ function AdminLogin({setToken}) {
     <div>
       <div className='container'>
         <div className='heder'>
-            <form onSubmit={UserSubmit}>
-                <input onChange={(e)=> setAdminEmail(e.target.value)} className='admin-email' type="email" />
-                <input onChange={(e)=> setAdminPassword(e.target.value)} className='admin-password' type="password" />
-                <button className='admin-sumit-button' type='submit'>submit</button>
-            </form>
+          <form onSubmit={UserSubmit}>
+            <input onChange={(e) => setAdminEmail(e.target.value)} className='admin-email' type="email" />
+            <input onChange={(e) => setAdminPassword(e.target.value)} className='admin-password' type="password" />
+            <button className='admin-sumit-button' type='submit'>submit</button>
+          </form>
         </div>
       </div>
     </div>
